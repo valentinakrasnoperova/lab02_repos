d0962517adff2fcdc29437325c2a5456214182e6
valentina <kresnoperova.valentina2003@gmail.com>
2020-10-11 23:31:09 +0300

    sixth commit in the second branch

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@ int main() {
     z1 = cos (x)* cos(x)* cos(x)* cos(x)* cos(x) + sin(x)*sin(x) + 1./4* sin(2*x)* sin(2*x) -1;
     //z2 = sin(y+x)*sin(y-x);
     //for second branch
+    // for second branch 2.0
+    // la ala al
     cout << endl;
     cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
